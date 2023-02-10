5d1dc6298b1b1eab04f0d2cc464e9abe588de9e4
mxrshall <martin22.krajcovic@gmail.com>
2023-02-10 03:38:07 +0100

    Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+const Navigation = () => {
+    return (
+      <div className="Navigation">
+        <div className="Navigation_Text"><a href="#domov">Domov</a></div>
+        <div className="Navigation_Text"><a href="#onas">Onas</a></div>
+      </div>
+    );
+  }
+  
+  export default Navigation;
